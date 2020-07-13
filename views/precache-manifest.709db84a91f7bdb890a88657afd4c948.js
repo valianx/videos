6611a1b553f4ea530ee12fc00f2515225e736d08
OscarJDesign <oscarj.larenas.nova@gmail.com>
2020-07-12 23:04:11 -0400

    'loginn'

---
+++ views/precache-manifest.709db84a91f7bdb890a88657afd4c948.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9366190193efb54dc599fa869266a812",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c11a54587fd9c6ab2141",
+    "url": "/static/css/main.cdd5d782.chunk.css"
+  },
+  {
+    "revision": "219ffa2b426c529a931a",
+    "url": "/static/js/2.dfa2ee3e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dfa2ee3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c11a54587fd9c6ab2141",
+    "url": "/static/js/main.e0717625.chunk.js"
+  },
+  {
+    "revision": "422a4b79b3340d44021a",
+    "url": "/static/js/runtime-main.6cc9b634.js"
+  },
+  {
+    "revision": "3a62d03a655b4a74d2e7bfdfee42c5d6",
+    "url": "/static/media/Video2.3a62d03a.mp4"
+  },
+  {
+    "revision": "70f19332427b29096c615f0b3b224141",
+    "url": "/static/media/logo2020.70f19332.png"
+  }
+]);
